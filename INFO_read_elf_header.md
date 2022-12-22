ff48cd619daf8a7eacd1af423f0e4804c0d2712e
Bastien Levasseur <57560210+ComradePusheen@users.noreply.github.com>
2022-12-22 22:17:47 +0100

    Fin Etape 2 [tests necéssaires]
    ===L'étape 2 doit encore passer la phase de tests===
    Finalisation de l'étape 2:
    •Les fonctionnalitées prévues (c.f. commit daf4eb9) ne sont pas ajoutées pour l'instant, elles sont reportées à plus tard
    
    Modifications de l'étape 1:
    •Ajout de commentaires pour toutes les fonctions et le main de cette étape
    •Mise à jour du INFO_read_elf_header.md
    •Suppression d'un case pour le type d'architecture qui n'était pas dans le cadre du projet (64 Bits)
    •Ajout d'un default case manquant pour le type d'architecture
    
    Modifications de l'étape 2:
    •Ajout de commentaires pour toutes les fonctions et le main de cette étape
    
    Modifications communes:
    •Ajout d'une fonction afficherAdresses qui affiche des adresses sur 7 bits avec le préfixe 0x
    •Ajout de commentaires pour les modules CheckELF
    
    •Mise à jour des informations du README

--- INFO_read_elf_header.md
+++ INFO_read_elf_header.md
@@ -1,13 +1,3 @@
-Les fichiers ELF sont des fichiers objets qui peuvent être:
-• Un executable
-• Un relocatable object (??)
-• Un objet partagé par d'autres fichiers objets
-
-
-Ces fichiers sont donc au format *binaire*, on cherche donc à lire octet par octet
-
-## Phase 1 : Etape 1 :
-
 On cherche à lire le header (en-tête) d'un .elf, le header est défini dans la _Spécification du Format ELF_ page 4
 On peut lire des fichiers binaires en C, on utilise l'option 'b' lors du fopen() pour spécifier un fichier binaire
 
@@ -23,22 +13,30 @@ adr     data
 .....
 On note la convention Big Endiant, il faut donc lire 7F 45 4C 46 ....
 
+##Lire avec readelf -h:
+
+Le but de cette étape est d'implémenter la même fonctionnalité que la commande readelf -h <FichierBinaire>
+On peut donc beaucoup s'aider de cette commande pour l'affichage.
+De plus, on considérera cette commande comme un oracle lors de la vérification automatique.
+
 ##Lire avec read_elf_header :
-En même temps que la lecture, on remplit une structure ELF_Header qui est la même que celle spécifiée dans elf.h
-Cette structure est créé avec un malloc mais *n'est pas libérée* à la fin de la lecture, il faudra donc penser à la libérer.
+En même temps que la lecture, on remplit une structure ELF_Header qui est la même que celle spécifiée dans elf.h.
 
 La lecture et l'affichage se fait avec le module freadoctet disponible dans le dossier modules.
+Le remplissage de la structure se fait avec le module readHeader disponible dans le dossier modules
 
 Le header commence par le magic number composé de 16 octets:
-	On l'affiche simplement pas besoin de le décoder
+	On l'affiche simplement pas besoin de le décoder.
 
-On continue ensuite pour toutes les autres structures en faisaint attention à au type:
-	Les informations codées sur un Word demandent de lire 4 octets
-	Les informations codées sur un Half(-Word) demandent de lire 2 octets
-	Les offsets sont équivalents aux Word et demandent donc 4 octets
+On continue ensuite pour tous les autres membres en faisaint attention au type:
+	Les informations codées sur un Word demandent de lire 4 octets.
+	Les informations codées sur un Half(-Word) demandent de lire 2 octets.
+	Les offsets sont équivalents aux Word et demandent donc 4 octets.
+	Les adresses sont équivalents aux Word et demandent aussi 4 octets.
 
 On lit le fichier de façon linéaire en incrémentant le pointeur de lecture à chaque read.
 Seule exception est après de la vérification du Magic Number où on retourne au début de fichier.
+
 
 
 ## Générer des fichiers .elf pour des tests :
@@ -51,3 +49,5 @@ clang --target=arm-v7-pc-linux-gnu -mfloat-abi=soft -c main.c
 
 Ou pour un i386:
 clang --target=i386-pc-linux-gnu -c main.c
+
+Ne pas générer des tests pour un processeur 64 bits, les ELF 64 bits sont en dehors du cadre du projet.
--- modules/readHeader.h
+++ modules/readHeader.h
@@ -11,47 +11,169 @@
 #include <stdio.h>
 #include "CustomElf.h"
 
+/* Lit le nombre magique d'un fichier ELF et le stocke dans la structure Header
+	Affiche le nombre suivant le paramètre verbose.
+	Le nombre magique est sur 16 octets, on lit donc 16 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute le magic number à e_indent
+ */
 void magicNumber(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit le type objet du fichier et le stocke dans la structure Header.
+	Affiche le type et le détail suivant le paramètre verbose.
+	Le type est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute le numéro de type à e_type (et pas le détail)
+ */
 void objectType(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit l'architecture de l'ELF et la stocke dans la structure Header.
+ Affiche l'architecture et le détail suivant le paramètre verbose.
+ L'architecture est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute le numéro de type à e_machine (et pas le détail)
+ */
 void machineType(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit la version de l'ELF et la stocke dans la structure Header.
+ Affiche la version et le détail suivant le paramètre verbose.
+ La version est un Word, on lit donc 4 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute le numéro de type à e_version (et pas le détail)
+ */
 void version(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit l'adresse d'entrée et la stocke dans la structure Header.
+ Affiche l'adresse suivant le paramètre verbose.
+ L'adresse est un Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute l'adresse à e_entry
+ */
 void entry(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit le décalage au début de programme et la stocke dans la structure Header.
+ Affiche le décalage suivant le paramètre verbose.
+ Le décalage est un Word, on lit donc 4 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_phoff
+ */
 void progHeaderOff(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit le décalage au début de section et la stocke dans la structure Header.
+ Affiche le décalage suivant le paramètre verbose.
+ Le décalage est un Word, on lit donc 4 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_shoff
+ */
 void sectHeaderOff(FILE *file, Elf32_Ehdr* Header, char verbose);
 
 
+/* Lit les flags prévus et les stockent dans la structure Header.
+ === ATTENTION: Le détail n'est pas (encore) disponible pour cette caractéristique, bien qu'il serait pratique...
+				Pour l'instant on affiche donc que la valeur qui n'est pas parlante par elle-même ===
+ Affiche le décalage suivant le paramètre verbose.
+ La valeur est un Word, on lit donc 4 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_flags (Les détails ne seront pas ajoutés)
+ */
 void flags(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit la taille de l'en-tête et la stocke dans la structure Header.
+ Affiche la taille suivant le paramètre verbose.
+ La taille est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_ehsize
+ */
 void headerSize(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit la taille de l'en-tête de chaque programme et la stocke dans la structure Header.
+ Affiche la taille suivant le paramètre verbose.
+ Le nombre est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_phentsize
+ */
 void headerProgramSize(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit le nombre d'en-têtes de programmes et le stocke dans la structure Header.
+ Affiche la taille suivant le paramètre verbose.
+ Le nombre est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_phnum
+ */
 void nbProgramHeaders(FILE *file,Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit la taille de la section de chaque programme et la stocke dans la structure Header.
+ Affiche la taille suivant le paramètre verbose.
+ La taille est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute la valeur à e_shentsize
+ */
 void sizeSectionHeaders(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit le nombre d'en-tête et le stocke dans la structure Header.
+ Affiche le nombre suivant le paramètre verbose.
+ La taille est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute le nombre à e_shnum
+ */
 void nbSectionHeader(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Lit l'index de la String Table et le stocke dans la structure Header.
+ Affiche l'index suivant le paramètre verbose.
+ Le nombre est un Half-Word, on lit donc 2 octets.
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, ajoute l'index à e_shstrndx
+ */
 void indexStringHeader(FILE *file, Elf32_Ehdr *Header, char verbose);
 
 
+/* Remplit la structure Header avec une fonction pour chaque membre
+
+ Paramètre: un pointeur file de fichier, un pointeur Header de structure d'en-tête, un booléen verbose pour gérer l'affichage
+ Sortie : Ne renvoie rien
+ Effets de bords : Modifie la structure Header, la remplie entièrement
+ */
 void getHeader(FILE *file, Elf32_Ehdr *Header, char verbose);
+
+
 #endif /* readHeader_h */
