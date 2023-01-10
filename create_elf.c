80511be777ce55550595b4c2d02b1633c5da5966
RonaldDomi <ronalddomi4@gmail.com>
2023-01-10 14:10:56 +0100

    Generation of sections and buffer prise en compte

--- create_elf.c
+++ create_elf.c
@@ -40,19 +40,6 @@ int main(int argc, char **argv){
 	fclose(file2);
 }
 
-// Elf32_SecContent
-// Elf32_Shdr
-// typedef struct {
-//     int nbSections;
-//     Elf32_Shdr * TabAllSec[50];
-//     Elf32_SecContent TabAllSecContent[50];
-// } Elf32_AllSec;
-
-// typedef struct {
-//     Elf32_Ehdr * Header;
-//     Elf32_AllSec * AllSections;
-//     Elf32_Sym * AllSymbol;
-// } Elf32_Info;
 void write_to_file(Elf32_Info* ELF1, FILE* file){
     write_header(ELF1->Header, file);
     write_sections(ELF1->AllSections, file);
@@ -63,6 +50,7 @@ void write_to_file(Elf32_Info* ELF1, FILE* file){
 
         int output[2] = {0, 0};
         fwrite(output, 1, 1, file);
+        // printf("adding one 0 ");
         current_pos = ftell(file);
     }
 
@@ -73,88 +61,125 @@ void write_to_file(Elf32_Info* ELF1, FILE* file){
 void write_header(Elf32_Ehdr* Header, FILE* file){
     printf("################## WRITING HEADER BYTES ##############\n\n");
 
-    printf("ident: ");
-    printOctet(&Header->e_ident, 16, 0);
+    // printf("ident: ");
+    // printOctet(&Header->e_ident, 16, 0);
     fwrite(&Header->e_ident, 1, 16, file);
-    printf("\n");
+    // printf("\n");
 
-    printf("type: ");
-    printOctet(&Header->e_type, 2, 0);
+    // printf("type: ");
+    // printOctet(&Header->e_type, 2, 0);
     fwrite(&Header->e_type, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_machine: ");
-    printOctet(&Header->e_machine, 2, 0);
+    // printf("e_machine: ");
+    // printOctet(&Header->e_machine, 2, 0);
     fwrite(&Header->e_machine, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_version: ");
-    printOctet(&Header->e_version, 4, 0);
+    // printf("e_version: ");
+    // printOctet(&Header->e_version, 4, 0);
     fwrite(&Header->e_version, 1, 4, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_entry: ");
-    printOctet(&Header->e_entry, 4, 0);
+    // printf("e_entry: ");
+    // printOctet(&Header->e_entry, 4, 0);
     fwrite(&Header->e_entry, 1, 4, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_phoff: ");
-    printOctet(&Header->e_phoff, 4, 0);
+    // printf("e_phoff: ");
+    // printOctet(&Header->e_phoff, 4, 0);
     fwrite(&Header->e_phoff, 1, 4, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_shoff: ");
-    printOctet(&Header->e_shoff, 4, 0);
+    // printf("e_shoff: ");
+    // printOctet(&Header->e_shoff, 4, 0);
     fwrite(&Header->e_shoff, 1, 4, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_flags: ");
-    printOctet(&Header->e_flags, 4, 0);
+    // printf("e_flags: ");
+    // printOctet(&Header->e_flags, 4, 0);
     fwrite(&Header->e_flags, 1, 4, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_ehsize: ");
-    printOctet(&Header->e_ehsize, 2, 0);
+    // printf("e_ehsize: ");
+    // printOctet(&Header->e_ehsize, 2, 0);
     fwrite(&Header->e_ehsize, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_phentsize: ");
-    printOctet(&Header->e_phentsize, 2, 0);
+    // printf("e_phentsize: ");
+    // printOctet(&Header->e_phentsize, 2, 0);
     fwrite(&Header->e_phentsize, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_phnum: ");
-    printOctet(&Header->e_phnum, 2, 0);
+    // printf("e_phnum: ");
+    // printOctet(&Header->e_phnum, 2, 0);
     fwrite(&Header->e_phnum, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_shentsize: ");
-    printOctet(&Header->e_shentsize, 2, 0);
+    // printf("e_shentsize: ");
+    // printOctet(&Header->e_shentsize, 2, 0);
     fwrite(&Header->e_shentsize, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_shnum: ");
-    printOctet(&Header->e_shnum, 2, 0);
+    // printf("e_shnum: ");
+    // printOctet(&Header->e_shnum, 2, 0);
     fwrite(&Header->e_shnum, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
-    printf("e_shstrndx: ");
-    printOctet(&Header->e_shstrndx, 2, 0);
+    // printf("e_shstrndx: ");
+    // printOctet(&Header->e_shstrndx, 2, 0);
     fwrite(&Header->e_shstrndx, 1, 2, file);
-    printf("\n");
+    // printf("\n");
     
 }
 
 void write_sections(Elf32_AllSec* Sections, FILE* file){
-    printf("\n################## WRITING HEADER BYTES ##############\n\n");
+    printf("\n################## WRITING SECTION BYTES ##############\n\n");
 
+    // get the write order of sections to write into order
+    int present[Sections->nbSections];
+    int order[Sections->nbSections];
     for(int i = 0; i<Sections->nbSections; i++){
-        printf("\nSize of section %d, %d\n", i, Sections->TabAllSec[i]->sh_size);
-        int size = Sections->TabAllSec[i]->sh_size;
+        present[i] = 0;
+        order[i] = i;
+    }
+
+    int min_pos = 0;
+    int min;
+    for(int i=0; i<Sections->nbSections; i++){
+        min = 1000000;
+        min_pos = i;
+        for(int j=0; j<Sections->nbSections; j++){
+            if(Sections->TabAllSec[j]->sh_offset < min){
+                // si il est pas present
+                if(present[j] == 0){
+                    min = Sections->TabAllSec[j]->sh_offset;
+                    min_pos = j;
+                }
+            }
+        }
+        present[min_pos] = 1;
+        order[i] = min_pos;
+    }
+
+    
+    for(int i = 0; i<Sections->nbSections; i++){
+        int size = Sections->TabAllSec[order[i]]->sh_size;
+        
         if(size != 0){
-            fwrite(Sections->TabAllSecContent[i], 1, size, file);
-            printOctet(Sections->TabAllSecContent[i], size, 0);
-            printf("\n");
+            fwrite(Sections->TabAllSecContent[order[i]], 1, size, file);
+            if(i+2 <= Sections->nbSections){
+                // difference is to know if we need buffer 0 so that the alignments match
+                int difference = Sections->TabAllSec[order[i+1]]->sh_offset - (Sections->TabAllSec[order[i]]->sh_offset + Sections->TabAllSec[order[i]]->sh_size);
+                if(difference > 0){
+                    // complete with additional 0
+                    while(difference > 0){
+                        int output[2] = {0, 0};
+                        fwrite(output, 1, 1, file);
+                        difference = difference - 1;
+                    }
+                }
+            }
         }
     }
 }
@@ -163,55 +188,55 @@ void write_section_header(Elf32_AllSec* Sections, FILE* file){
     printf("\n################## WRITING SECTION HEADER BYTES ##############\n\n");
     for(int i = 0; i<Sections->nbSections; i++){
         Elf32_Shdr* SectionTable = Sections->TabAllSec[i];
-        printf("sh_name: ");
-        printOctet(&SectionTable->sh_name, 4, 0);
+        // printf("sh_name: ");
+        // printOctet(&SectionTable->sh_name, 4, 0);
         fwrite(&SectionTable->sh_name, 1, 4, file);
-        printf("\n");
+        // printf("\n");
 
-        printf("sh_type: ");
-        printOctet(&SectionTable->sh_type, 4, 0);
+        // printf("sh_type: ");
+        // printOctet(&SectionTable->sh_type, 4, 0);
         fwrite(&SectionTable->sh_type, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_flags: ");
-        printOctet(&SectionTable->sh_flags, 4, 0);
+        // printf("sh_flags: ");
+        // printOctet(&SectionTable->sh_flags, 4, 0);
         fwrite(&SectionTable->sh_flags, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_addr: ");
-        printOctet(&SectionTable->sh_addr, 4, 0);
+        // printf("sh_addr: ");
+        // printOctet(&SectionTable->sh_addr, 4, 0);
         fwrite(&SectionTable->sh_addr, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_offset: ");
-        printOctet(&SectionTable->sh_offset, 4, 0);
+        // printf("sh_offset: ");
+        // printOctet(&SectionTable->sh_offset, 4, 0);
         fwrite(&SectionTable->sh_offset, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_size: ");
-        printOctet(&SectionTable->sh_size, 4, 0);
+        // printf("sh_size: ");
+        // printOctet(&SectionTable->sh_size, 4, 0);
         fwrite(&SectionTable->sh_size, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_link: ");
-        printOctet(&SectionTable->sh_link, 4, 0);
+        // printf("sh_link: ");
+        // printOctet(&SectionTable->sh_link, 4, 0);
         fwrite(&SectionTable->sh_link, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_info: ");
-        printOctet(&SectionTable->sh_info, 4, 0);
+        // printf("sh_info: ");
+        // printOctet(&SectionTable->sh_info, 4, 0);
         fwrite(&SectionTable->sh_info, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_addralign: ");
-        printOctet(&SectionTable->sh_addralign, 4, 0);
+        // printf("sh_addralign: ");
+        // printOctet(&SectionTable->sh_addralign, 4, 0);
         fwrite(&SectionTable->sh_addralign, 1, 4, file);
-        printf("\n");
+        // printf("\n");
         
-        printf("sh_entsize: ");
-        printOctet(&SectionTable->sh_entsize, 4, 0);
+        // printf("sh_entsize: ");
+        // printOctet(&SectionTable->sh_entsize, 4, 0);
         fwrite(&SectionTable->sh_entsize, 1, 4, file);
-        printf("\n");
+        // printf("\n");
     }
 
 }
